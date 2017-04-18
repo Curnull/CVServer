1e894307e49cdcc6ddf09e6f6ed3f52a06529ad7
danielsemenov <d.semenov@velvetech.ru>
2017-04-18 17:32:11 +0700

    added CV provider

---
+++ cvProvider.js
@@ -0,0 +1,67 @@
+var rimraf = require('rimraf');
+var babel = require("babel-core");
+var glob = require("glob");
+var fs = require('fs');
+var path =  require('path');
+
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.split(search).join(replacement);
+};
+
+function transfromFile(input, output){
+  return new Promise(function(resolve, reject) {
+    console.log("compiling " + input);
+    babel.transformFile(input, {}, function(err, result){
+      if(err){
+        reject(err);
+        return;
+      }
+      var fileName = path.basename(output);
+      var cvName = fileName.substring(0, fileName.indexOf('.'));
+      var codeToSave = result.code.replaceAll("{server_url}", server_url).replaceAll('{cv_name}', cvName);
+      fs.writeFile(output, codeToSave, function(err) {
+        if(err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+var server_url = '';
+
+module.exports = {
+  init: function(_server_url) {
+    server_url = _server_url;
+    rimraf('./data', function () {
+      console.log('"data" folder was removed');
+      fs.mkdirSync("./data");
+      glob("./data_src/*.js", null, function(err, filenames){
+        if (err) {
+          throw err;
+        }
+        for(var index in filenames){
+          var path = filenames[index];
+          var out = path.replace("_src", "");
+          transfromFile(path, out).then(function(){
+            console.log("The " + out + " was saved!");
+          }).catch(function(err) {
+            throw new Error(err);
+          });
+        }
+      });
+    });
+  },
+  getCV: function(name, lang) {
+    return new Promise(function(resolve, reject) {
+      var out = "./data/" + name + ".js";
+      transfromFile('./data_src/' + name + '.js', out).then(function(){
+        delete require.cache[require.resolve(out)]
+        resolve(require(out).default(lang));
+      });
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,48 +1,21 @@
 var path =  require('path');
 var express =  require('express');
 var cors = require('cors');
-var rimraf = require('rimraf');
-var babel = require("babel-core");
-var glob = require("glob");
-var fs = require('fs');
-
-function transfromFile(input, output){
-  console.log("compiling " + input);
-  babel.transformFile(input, {}, function(err, result){
-    if(err){
-      throw err;
-    }
-    fs.writeFile(output, result.code, function(err) {
-      if(err) {
-        throw err;
-      }
-      console.log("The " + output + " was saved!");
-    });
-  });
-}
-
-
-//build CV's
-rimraf('./data', function () {
-  console.log('"data" folder was removed');
-  fs.mkdirSync("./data");
-  glob("./data_src/*.js", null, function(err, filenames){
-    if (err) {
-      throw err;
-    }
-    for(var index in filenames){
-      var path = filenames[index];
-      transfromFile(path, path.replace("_src", ""));
-    }
-  });
-});
-
+var cvProvider = require('./cvProvider');
+var os = require('os');
 
 var port = 3000;
+var server_url = 'http://' + os.hostname() + ':' + port;
+cvProvider.init(server_url);
+
+
 var app = express();
 app.use(cors());
-app.get('/cv/:name', function (req, res) {
-  res.send(require("./data/" + req.params.name + ".js").default);
+app.get('/cv/:name/:lang', function (req, res) {
+  cvProvider.getCV(req.params.name, req.params.lang || 'en').then(function(cv){
+    res.send(cv);
+  });
+
 });
 
 app.get('/img/:filename', function(req, res){
